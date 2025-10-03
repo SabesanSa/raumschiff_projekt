a36e950ac39cc956bc117b6876d4662db8472c2a
SabesanSa <sabesis@web.de>
2025-10-03 16:15:53 +0200

    Test

--- Raumschiff.java
+++ Raumschiff.java
@@ -9,27 +9,28 @@ public class Raumschiff
 {
   GLKugel körper;
   GLQuader flügel1;
-  GLKugel accesoire1;
+  GLKugel accessoire1;
   GLQuader flügel2;
   GLKugel accessoire2;
-  
-  
+  int x;
+   
  public Raumschiff (){
    körper = new GLKugel(0,0,0,50);
    körper.setzeSkalierung(3,1,3);
-   //körper.setzeTextur("RaumschiffTextur.jpg");
+   körper.setzeTextur("RaumschiffTextur.jpg");
    
    flügel1 = new GLQuader(100,0,100,70,25,150);
-   
+   flügel1.setzeTextur("RaumschiffTextur.jpg");
+
    flügel2 = new GLQuader(-100,0,100,70,25,150);
-   //flügel1.setzeTextur("RaumschiffTextur.jpg");
+   flügel2.setzeTextur("RaumschiffTextur.jpg");
    
    
-   accesoire1 = new GLKugel(100,0,100,25);
-   //flügel2.setzeTextur("RaumschiffTextur.jpg");
+   accessoire1 = new GLKugel(100,0,100,25);
+   accessoire1.setzeTextur("RaumschiffTextur.jpg");
    
    accessoire2 = new GLKugel(-100,0,100,25);
-   //accessoir2.setzeTextur("RaumschiffTextur.jpg");
+   accessoire2.setzeTextur("RaumschiffTextur.jpg");
       
  }
 }
